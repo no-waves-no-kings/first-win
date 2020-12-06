07f2c1baefe24bc419c407444b8461f33632f98a
sethcraw <543107476@qq.com>
2020-12-06 21:51:44 +0800

    feat(all): no-waves-no-kings,first-win,first commit
    不浪难上王者，首胜项目第一次提交，为了让打野和项羽可以进入项目，共同合作

---
+++ src/main/java/com/nowavesnokings/firstwin/FirstwinApplication.java
@@ -0,0 +1,13 @@
+package com.nowavesnokings.firstwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstwinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstwinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nowavesnokings/firstwin/FirstwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowavesnokings.firstwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstwinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
