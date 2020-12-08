cb2fafb71745eec4e2636d054182c244846d9b69
sethcraw <543107476@qq.com>
2020-12-08 16:02:50 +0800

    refactor(framwork add package): add package
    添加包文件·

---
+++ src/main/java/com/nowavesnokings/firstwin/v1/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.nowavesnokings.firstwin.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ssx
+ * @version V1.0
+ * @className HelloWorldController
+ * @description 测试api
+ * @date 2020-12-08 15:54
+ * @since 1.8
+ */
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
